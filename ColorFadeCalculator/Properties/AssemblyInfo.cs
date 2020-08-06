fdfb226e70f6b77c7fa48241d94ef229f3257f9e
cloudd901 <cloudd901@gmail.com>
2020-08-05 23:43:52 -0500

    Update/Rename

--- ColorFadeCalculator/Properties/AssemblyInfo.cs
+++ ColorFadeCalculator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PCA ColorFadeCalculator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ColorFadeCalculator")]
+[assembly: AssemblyDescription("Easily calculate color fading between two colors.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PC Affinity")]
-[assembly: AssemblyProduct("FadeCalculator")]
+[assembly: AssemblyProduct("ColorFadeCalculator")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
